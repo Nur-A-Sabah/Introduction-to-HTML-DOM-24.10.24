024316f3e961872e5fee0c8b996f62d6e4c30432
Nur-A-Sabah <sajedurkabir@gmail.com>
2024-10-29 09:02:32 -0700

    acp

---
+++ ACP 24.10.2024/script.js
@@ -0,0 +1,16 @@
+function myfunction() {
+  var my = {
+    name: "Nur-A-Sabah",
+    age: 15,
+    grade: 8,
+    address: "Kantha baria , Rajshahi",
+    shool: "Mission Girls High School",
+    fav_colors: ["White", " Black", " Purple"],
+  };
+  document.getElementById("name").innerHTML = my.name;
+  document.getElementById("age").innerHTML = my.age;
+  document.getElementById("grade").innerHTML = my.grade;
+  document.getElementById("address").innerHTML = my.address;
+  document.getElementById("school").innerHTML = my.shool;
+  document.getElementById("fav_colors").innerHTML = my.fav_colors;
+}
